468fbef4aa90deda2f24b46ec25b5bc46e441213
KHSenevirathne <khsenevirathne@gmail.com>
2023-01-31 14:43:22 +0530

    done

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,14 @@
 import java.io.IOException;
 import java.net.Socket;
 
+
 public class Client {
     public static void main(String[] args) {
         
         try {
-            System.out.println("Client Started");
-            Socket s = new Socket("localhost",9806);
+            Socket s = new Socket("localhost", 6609);
+            System.out.println("Client Side Started");
         } catch (IOException e) {
-            
             e.printStackTrace();
         }
     }
--- src/Server.java
+++ src/Server.java
@@ -2,16 +2,16 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server{
+public class Server {
     public static void main(String[] args) {
-
+        
         try {
-            System.out.println("Waiting for clients requests");
-            ServerSocket socket = new ServerSocket(9806);
+            System.out.println("Server Side Started.");
+            ServerSocket socket = new ServerSocket(6609);
             Socket s = socket.accept();
+            System.out.println("Connection Established.");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Connection is Established");
     }
 }
