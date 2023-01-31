da5021cb91a4d9c48385a991b7af958fdfd5f767
KHSenevirathne <khsenevirathne@gmail.com>
2023-01-31 13:35:44 +0530

    change

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,14 @@
-public class Server {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Server{
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Waiting for clients requests");
+            ServerSocket socket = new ServerSocket(9806);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
