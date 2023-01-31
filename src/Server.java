0f8d3444d00357f224b67913e42f14aa620b9805
KHSenevirathne <khsenevirathne@gmail.com>
2023-01-31 13:42:55 +0530

    Server Completed

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server{
     public static void main(String[] args) {
@@ -7,8 +8,10 @@ public class Server{
         try {
             System.out.println("Waiting for clients requests");
             ServerSocket socket = new ServerSocket(9806);
+            Socket s = socket.accept();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Connection is Established");
     }
 }
